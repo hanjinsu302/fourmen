6b4787f34152fd610cc95891da33ddabf67f61f4
Hanjinsu302 <jinsuhan302@gmail.com>
2023-03-24 12:10:03 +0900

    chelsea

--- mainpage/app.js
+++ mainpage/app.js
@@ -1,75 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 4040;
-app.set('view engine','ejs');
-app.set('/view','views');
-app.use('/static',express.static(__dirname + '/static'));
+app.set("view engine", "ejs");
+app.set("/view", "views");
+app.use("/static", express.static(__dirname + "/static"));
 
-app.get('/',function(request,response){
-    response.render('index');
+app.get("/", function (request, response) {
+  response.render("index");
 });
 
-app.get('/index',function(request,response){
-    response.render('index');
+app.get("/index", function (request, response) {
+  response.render("index");
 });
 
-app.get('/matches', function(req, res) {
-    res.render('matches');
-})
-
-app.get('/index/matches', function(req,res){
-
-    res.render('matches.ejs');
-
-})
-
-
-app.get('/players', function(req, res) {
-    res.render('players');
-})
-
-app.get('/index/players', function(req,res){
-
-    res.render('players.ejs');
-
-})
-
-
-app.get('/blog', function(req, res) {
-    res.render('blog');
-})
-
-app.get('/index/blog', function(req,res){
-
-    res.render('blog.ejs');
-
-})
-
-
-app.get('/contact', function(req, res) {
-    res.render('contact');
-})
-
-app.get('/index/contact', function(req,res){
-
-    res.render('contact.ejs');
-
-})
-
-app.get('/tottenum', function(req, res) {
-    res.render('tottenum');
-})
-
-app.get('/index/tottenum', function(req,res){
-
-    res.render('tottenum.ejs');
-
-})
-
-
-
-app.listen(port,()=>{
-    console.log(port+" is open!");
+app.get("/matches", function (req, res) {
+  res.render("matches");
 });
 
+app.get("/index/matches", function (req, res) {
+  res.render("matches.ejs");
+});
+
+app.get("/players", function (req, res) {
+  res.render("players");
+});
+
+app.get("/index/players", function (req, res) {
+  res.render("players.ejs");
+});
+
+app.get("/blog", function (req, res) {
+  res.render("blog");
+});
+
+app.get("/index/blog", function (req, res) {
+  res.render("blog.ejs");
+});
+
+app.get("/chelsea", function (req, res) {
+  res.render("chelsea");
+});
+
+app.get("/index/chelsea", function (req, res) {
+  res.render("chelsea.ejs");
+});
+
+app.get("/tottenum", function (req, res) {
+  res.render("tottenum");
+});
+
+app.get("/index/tottenum", function (req, res) {
+  res.render("tottenum.ejs");
+});
+
+app.listen(port, () => {
+  console.log(port + " is open!");
+});
