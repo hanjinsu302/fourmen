2ba1f3918dad322c2a4a02dea17b1f5073a00ba1
onenationonemind1 <rkfmvk1117@gmail.com>
2023-03-24 18:25:33 +0900

    matches -1

---
+++ mainpage/static/js/staticAnimation.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements1 = document.querySelectorAll(".hidden1");
+hiddenElements1.forEach((el1) => observer.observe(el1));
